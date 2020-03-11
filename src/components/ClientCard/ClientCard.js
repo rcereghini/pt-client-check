fd9c801e1d5bc4abc447c77b186086338dfdfcb0
Robert Cereghini <rcereghini@gmail.com>
2020-03-10 21:42:14 -0700

    Client List Modal working and Component repurposed to inner content

--- src/components/ClientCard/ClientCard.js
+++ src/components/ClientCard/ClientCard.js
@@ -5,6 +5,23 @@ import Paper from "@material-ui/core/Paper";
 import ClientListModal from "../ClientListModal/ClientListModal";
 import user from "../../models/user";
 
+import Modal from "@material-ui/core/Modal";
+
+function rand() {
+  return Math.round(Math.random() * 20) - 10;
+}
+
+function getModalStyle() {
+  const top = 50 + rand();
+  const left = 50 + rand();
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`
+  };
+}
+
 const useStyles = makeStyles(theme => ({
   root: {
     display: "flex",
@@ -14,11 +31,31 @@ const useStyles = makeStyles(theme => ({
       width: theme.spacing(16),
       height: theme.spacing(16)
     }
+  },
+  paper: {
+    position: "absolute",
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3)
   }
 }));
 
 const ClientCard = props => {
-  console.log("props.client =>", props.client);
+  const classes = useStyles();
+  // getModalStyle is not a pure function, we roll the style only on the first render
+  const [modalStyle] = React.useState(getModalStyle);
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <Paper
       elevation={3}
@@ -29,6 +66,7 @@ const ClientCard = props => {
         margin: ".5em"
       }}
       className="client-card-main"
+      onClick={handleOpen}
     >
       <p>{props.client.name}</p>
       <p>
@@ -37,7 +75,19 @@ const ClientCard = props => {
       <p>
         <a href={`mail:${props.client.email}`}>{props.client.email}</a>
       </p>
-      <ClientListModal></ClientListModal>
+      <Modal
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+        open={open}
+        onClose={handleClose}
+      >
+        <div style={modalStyle} className={classes.paper}>
+          <h2 id="simple-modal-title">Text in a modal</h2>
+          <p id="simple-modal-description">
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </p>
+        </div>
+      </Modal>
     </Paper>
   );
 };
