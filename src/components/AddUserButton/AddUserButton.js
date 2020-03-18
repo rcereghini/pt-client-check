011cf38e33140168bd35141197642bc512039944
Robert Cereghini <rcereghini@gmail.com>
2020-03-17 22:09:28 -0700

    Add friend modal cleanup

--- src/components/AddUserButton/AddUserButton.js
+++ src/components/AddUserButton/AddUserButton.js
@@ -5,49 +5,11 @@ import Paper from "@material-ui/core/Paper";
 import PersonAddIcon from "@material-ui/icons/PersonAdd";
 
 import Modal from "@material-ui/core/Modal";
-import { makeStyles } from "@material-ui/core/styles";
 
 import AddUserForm from "../AddUserForm/AddUserForm";
 
-function rand() {
-  return Math.round(Math.random() * 20) - 10;
-}
-
-function getModalStyle() {
-  const top = 50 + rand();
-  const left = 50 + rand();
-
-  return {
-    top: `${top}%`,
-    left: `${left}%`,
-    transform: `translate(-${top}%, -${left}%)`
-  };
-}
-
-const useStyles = makeStyles(theme => ({
-  paper: {
-    position: "absolute",
-    width: 400,
-    backgroundColor: theme.palette.background.paper,
-    border: "2px solid #000",
-    boxShadow: theme.shadows[5],
-    padding: theme.spacing(2, 4, 3)
-  },
-  root: {
-    display: "flex",
-    flexWrap: "wrap",
-    "& > *": {
-      margin: theme.spacing(1),
-      width: theme.spacing(16),
-      height: theme.spacing(16)
-    }
-  }
-}));
-
 const AddUserButton = () => {
-  const classes = useStyles();
   // getModalStyle is not a pure function, we roll the style only on the first render
-  const [modalStyle] = React.useState(getModalStyle);
   const [open, setOpen] = React.useState(false);
 
   const handleOpen = () => {
@@ -72,10 +34,15 @@ const AddUserButton = () => {
         aria-describedby="simple-modal-description"
         open={open}
         onClose={handleClose}
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center"
+        }}
       >
-        <div style={modalStyle} className={classes.paper}>
-          <AddUserForm></AddUserForm>
-        </div>
+        <Paper>
+          <div style={{ padding: "3em" }}>Add Friend Form</div>
+        </Paper>
       </Modal>
     </div>
   );
