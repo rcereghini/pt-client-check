9814a7288e79fc1d1657544dbb3c66a968a24736
Robert Cereghini <rcereghini@gmail.com>
2020-03-17 22:06:18 -0700

    Client modal cleanup

--- src/components/ClientCard/ClientCard.js
+++ src/components/ClientCard/ClientCard.js
@@ -11,17 +11,6 @@ function rand() {
   return Math.round(Math.random() * 20) - 10;
 }
 
-function getModalStyle() {
-  const top = 50 + rand();
-  const left = 50 + rand();
-
-  return {
-    top: `${top}%`,
-    left: `${left}%`,
-    transform: `translate(-${top}%, -${left}%)`
-  };
-}
-
 const useStyles = makeStyles(theme => ({
   paper: {
     position: "absolute",
@@ -45,7 +34,6 @@ const useStyles = makeStyles(theme => ({
 const ClientCard = props => {
   const classes = useStyles();
   // getModalStyle is not a pure function, we roll the style only on the first render
-  const [modalStyle] = React.useState(getModalStyle);
   const [open, setOpen] = React.useState(false);
 
   const handleOpen = () => {
@@ -82,13 +70,15 @@ const ClientCard = props => {
         aria-describedby="simple-modal-description"
         open={open}
         onClose={handleClose}
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center"
+        }}
       >
-        <div style={modalStyle} className={classes.paper}>
-          <h2 id="simple-modal-title">Text in a modal</h2>
-          <p id="simple-modal-description">
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </p>
-        </div>
+        <Paper>
+          <div style={{ padding: "3em" }}>Friend Info</div>
+        </Paper>
       </Modal>
     </div>
   );
